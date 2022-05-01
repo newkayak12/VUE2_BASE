b95b0fc31b8c99a49f8097f4dd14c2729f8c0cb1
newkayak12 <newkayak12@gmail.com>
2022-05-01 18:34:08 +0900

    spinner
    (cherry picked from commit 4c9a6b2a63adff11b04a34a32971ad151b95a1be)

---
+++ src/layouts/spinner/Spinner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Spinner",
+  computed:{
+    spinner(){
+      return this.$store.getters('layout/getSpinner')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
