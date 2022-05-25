6937020e475004ee3155795147b9b14aac89a277
newkayak12 <newkayak12@gmail.com>
2022-05-25 21:57:40 +0900

    meta

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+import Env from '@/common/constants/Env.json'
 module.exports = {
+    baseName: Env.PROJECT_NAME,
+    touchIconUrl:Env.TOUCH_ICON_PATH,
     devServer:{
-
         port:9001,
         proxy:{
             '/api':{
